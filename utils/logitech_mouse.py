9cb4a024db764f3e99aa688097590af00b7f83a7
bora <75948295+Seconb@users.noreply.github.com>
2024-12-19 22:04:56 -0500

    root finding improvements

--- utils/logitech_mouse.py
+++ utils/logitech_mouse.py
@@ -1,32 +1,28 @@
 import ctypes
-from time import sleep
 import win32file
-import win32api
-import ctypes
 import ctypes.wintypes as wintypes
 from ctypes import windll
 
 handle = 0
 found = False
+
 def _DeviceIoControl(devhandle, ioctl, inbuf, inbufsiz, outbuf, outbufsiz):
     """See: DeviceIoControl function
     http://msdn.microsoft.com/en-us/library/aa363216(v=vs.85).aspx
     """
     DeviceIoControl_Fn = windll.kernel32.DeviceIoControl
-    DeviceIoControl_Fn.argtypes = [wintypes.HANDLE, wintypes.DWORD, wintypes.LPVOID,wintypes.DWORD,wintypes.LPVOID,wintypes.DWORD,ctypes.POINTER(wintypes.DWORD),wintypes.LPVOID]                   
+    DeviceIoControl_Fn.argtypes = [wintypes.HANDLE, wintypes.DWORD, wintypes.LPVOID, wintypes.DWORD, wintypes.LPVOID, wintypes.DWORD, ctypes.POINTER(wintypes.DWORD), wintypes.LPVOID]
     DeviceIoControl_Fn.restype = wintypes.BOOL
 
     dwBytesReturned = wintypes.DWORD(0)
     lpBytesReturned = ctypes.byref(dwBytesReturned)
 
-    status = DeviceIoControl_Fn(int(devhandle),ioctl,inbuf,inbufsiz,outbuf,outbufsiz,lpBytesReturned,None)
+    status = DeviceIoControl_Fn(int(devhandle), ioctl, inbuf, inbufsiz, outbuf, outbufsiz, lpBytesReturned, None)
 
     return status, dwBytesReturned
 
-
 class MOUSE_IO(ctypes.Structure):
-    _fields_ = [("button", ctypes.c_char),("x", ctypes.c_char),("y", ctypes.c_char),("wheel", ctypes.c_char),("unk1", ctypes.c_char),]
-
+    _fields_ = [("button", ctypes.c_char), ("x", ctypes.c_char), ("y", ctypes.c_char), ("wheel", ctypes.c_char), ("unk1", ctypes.c_char)]
 
 def device_initialize(device_name: str) -> bool:
     global handle
@@ -36,22 +32,26 @@ def device_initialize(device_name: str) -> bool:
         pass
     return bool(handle)
 
+
 def mouse_open() -> bool:
     global found
     global handle
-    if handle: 
+
+    if handle:
         return found
-    if device_initialize('\\??\\ROOT#SYSTEM#0002#{1abc05c0-c378-41b9-9cef-df1aba82b015}'): #Would be best to put this in a for loop and only change the 0002
+
+    for i in range(1, 10):
+        device_name = f'\\??\\ROOT#SYSTEM#{str(i).zfill(4)}#{{1abc05c0-c378-41b9-9cef-df1aba82b015}}'
+        if device_initialize(device_name):
             found = True
-    else:
-        if device_initialize('\\??\\ROOT#SYSTEM#0001#{1abc05c0-c378-41b9-9cef-df1aba82b015}'): #Logitech CVE thank you ekknod
-            found = True
+            break
 
     return found
 
+
 def call_mouse(buffer) -> bool:
     global handle
-    return _DeviceIoControl(handle, 0x2a2010, ctypes.c_void_p(ctypes.addressof(buffer)), ctypes.sizeof(buffer),  0, 0)[0] == 0
+    return _DeviceIoControl(handle, 0x2a2010, ctypes.c_void_p(ctypes.addressof(buffer)), ctypes.sizeof(buffer), 0, 0)[0] == 0
 
 
 def mouse_close() -> None:
@@ -59,12 +59,13 @@ def mouse_close() -> None:
     win32file.CloseHandle(int(handle))
     handle = 0
 
+
 def mouse_move(button, x, y, wheel) -> None:
     global handle
 
     io = MOUSE_IO()
-    io.x = bytes([x % 256])  # Convert to single-byte value
-    io.y = bytes([y % 256])  # Convert to single-byte value
+    io.x = bytes([x % 256])
+    io.y = bytes([y % 256])
     io.unk1 = 0
     io.button = bytes([button % 256])
     io.wheel = bytes([wheel % 256])
@@ -73,5 +74,8 @@ def mouse_move(button, x, y, wheel) -> None:
         mouse_close()
         mouse_open()
 
+
 if not mouse_open():
     print("[-] G Hub is not open, or there is another issue! Download LG Hub at: https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer_2021.3.5164.exe and disable automatic updates.")
+else:
+    print("[+] G Hub is functioning as expected.")
